b3854fc8e1a32accf8082072d57be3b1d20ae7a7
张遥 <1870476411@qq.com>
2025-01-10 00:31:03 +0800

    公共字段自动填充
    https://github.com/zhangyao0329/tuchuang-blog/raw/main/tuchuang-blog/2025/01/09-0825e20ce27a3c9b96bb7829c350663b-image-20250109225427761.png

---
+++ sky-server/src/main/java/com/sky/annotation/AutoFill.java
@@ -0,0 +1,18 @@
+package com.sky.annotation;
+
+import com.sky.enumeration.OperationType;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 自定义注解，用于表示某个方法需要进行公共字段的自动填充。
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface AutoFill {
+    //    指定当前数据库操作数据库的类型(update insert)，通过枚举的方式来指定
+    OperationType value();
+
+}
--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -24,6 +26,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @AutoFill(value = OperationType.INSERT)
     @Insert("INSERT INTO employee " +
             "(name, username, password, phone, sex, id_number, status, create_time, update_time, create_user, update_user) " +
             "values " +
@@ -41,6 +44,7 @@ public interface EmployeeMapper {
     /**
      * 根据主键动态修改属性
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
