fefa2c8abc4434dba1bc7a714658516b55b1253b
张遥 <1870476411@qq.com>
2025-03-31 21:21:16 +0800

    工作台业务模块

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -6,6 +6,7 @@ import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
@@ -15,12 +16,14 @@ public interface OrderMapper {
 
     /**
      * 插入订单数据
+     *
      * @param orders
      */
     void insert(Orders orders);
 
     /**
      * 根据订单号和用户id查询订单
+     *
      * @param orderNumber
      * @param userId
      */
@@ -29,18 +32,21 @@ public interface OrderMapper {
 
     /**
      * 修改订单信息
+     *
      * @param orders
      */
     void update(Orders orders);
 
     /**
      * 分页条件查询并按下单时间排序
+     *
      * @param ordersPageQueryDTO
      */
     Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
 
     /**
      * 根据id查询订单
+     *
      * @param id
      */
     @Select("select * from orders where id=#{id}")
@@ -48,6 +54,7 @@ public interface OrderMapper {
 
     /**
      * 根据状态统计订单数量
+     *
      * @param status
      */
     @Select("select count(id) from orders where status = #{status}")
@@ -55,6 +62,7 @@ public interface OrderMapper {
 
     /**
      * 根据订单状态和下单时间查询订单
+     *
      * @param status
      * @param orderTime
      * @return
@@ -64,6 +72,7 @@ public interface OrderMapper {
 
     /**
      * 根据动态条件统计营业额数据
+     *
      * @param map
      * @return
      */
@@ -71,6 +80,7 @@ public interface OrderMapper {
 
     /**
      * 根据动态条件统计订单数量
+     *
      * @param map
      * @return
      */
@@ -78,9 +88,10 @@ public interface OrderMapper {
 
     /**
      * 统计指定时间区间内的销量排名前10
+     *
      * @param begin
      * @param end
      * @return
      */
-    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin,LocalDateTime end);
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
